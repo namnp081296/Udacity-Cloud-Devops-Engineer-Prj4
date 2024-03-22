83e280fb4d3dc511c1d81682341bf37b37f7314d
Nam Nguyen Phuong <namnp081296@gmail.com>
2024-03-22 21:58:20 +0700

    Change location github source CodeBuild resource

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -275,7 +275,7 @@ resource "aws_codebuild_project" "codebuild" {
 
   source {
     type            = "GITHUB"
-    location        = "https://github.com/your-org/your-repo"
+    location        = "https://github.com/namnp081296/Udacity-Cloud-Devops-Engineer-Prj4"
     git_clone_depth = 1
     buildspec       = "buildspec.yml"
   }
